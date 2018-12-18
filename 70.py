c5b106a82f30dcaa1cf8df7b18e15808c2f9e58a
qqq2830 <qqq2830@163.com>
2018-12-18 14:23:42 +0800

    70

---
+++ 70.py
@@ -0,0 +1,22 @@
+def sort(num):
+    return sorted(str(num))
+
+
+def compute(n):
+    result = list(range(n + 1))
+    for i in range(2, len(result)):
+        if result[i] == i:
+            for j in range(i, len(result), i):
+                result[j] -= result[j]//i
+    min = 100.0
+    n = 0
+    for i in range(3,len(result)):
+        if sort(i) == sort(result[i]):
+            if float(i) / result[i] < min:
+                min = float(i)/result[i]
+                n = i
+    return n,min
+
+
+if __name__ == "__main__":
+    print(compute(10**7))
--- 72.py
+++ 72.py
@@ -1,27 +1,11 @@
+import eulerlib, itertools
 
-def compute(n):
-    result = list(range(n + 1))
-    print(result)
-    for i in range(2, len(result)):
-        if result[i] == i:
-            for j in range(i, len(result), i):
-                result[j] -= result[j]//i
-    print(result)
-    return sum(result)
+
+def compute():
+	totients = eulerlib.list_totients(9)
+	ans = sum(itertools.islice(totients, 2, None))
+	return str(ans)
 
 
 if __name__ == "__main__":
-    print(compute(9))
-
-
-# import eulerlib, itertools
-#
-#
-# def compute():
-# 	totients = eulerlib.list_totients(9)
-# 	ans = sum(itertools.islice(totients, 2, None))
-# 	return str(ans)
-#
-#
-# if __name__ == "__main__":
-# 	print(compute())
+	print(compute())
