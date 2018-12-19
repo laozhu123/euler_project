9ddd3224b7409b2d178beae52cd0a3599a14b5b5
qqq2830 <qqq2830@163.com>
2018-12-19 21:02:11 +0800

    ?

--- 70.py
+++ 70.py
@@ -7,16 +7,16 @@ def compute(n):
     for i in range(2, len(result)):
         if result[i] == i:
             for j in range(i, len(result), i):
-                result[j] -= result[j]//i
+                result[j] -= result[j] // i
     min = 100.0
     n = 0
-    for i in range(3,len(result)):
+    for i in range(3, len(result)):
         if sort(i) == sort(result[i]):
             if float(i) / result[i] < min:
-                min = float(i)/result[i]
+                min = float(i) / result[i]
                 n = i
-    return n,min
+    return n, min
 
 
 if __name__ == "__main__":
-    print(compute(10**7))
+    print(compute(10 ** 7))
